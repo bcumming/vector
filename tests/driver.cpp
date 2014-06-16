987b3b6f18f54aa2a3d48fb6b7b8fa9665cf8789
bcumming <bcumming@cscs.ch>
2014-06-16 10:31:57 +0200

    fix cuda compilation flags and test for CUDA before running google tests

--- tests/driver.cpp
+++ tests/driver.cpp
@@ -4,7 +4,25 @@
 
 #include "gtest.h"
 
+#ifdef WITH_CUDA
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+bool initialize_cuda() {
+    CUresult result = cuInit(0);
+    return result == CUDA_SUCCESS;
+}
+#endif
+
 int main(int argc, char **argv) {
+    #ifdef WITH_CUDA
+    // if testing with CUDA turned on, first check that we can initialize CUDA
+    if(!initialize_cuda()) {
+        std::cerr << "ERROR: unable to initialize CUDA" << std::endl;
+        return 1;
+    }
+    #endif
+
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
