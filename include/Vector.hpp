a122798dd1c0c35f7658e45a91f1692d252e296d
bcumming <bcumming@cscs.ch>
2015-11-16 14:17:48 +0100

    add basic ostream support and make SplitRange use random access iterator

--- include/Vector.hpp
+++ include/Vector.hpp
@@ -19,6 +19,15 @@ using HostVector = Array<T, HostCoordinator<T>>;
 template <typename T>
 using HostView = ArrayView<T, HostCoordinator<T>>;
 
+template <typename T>
+std::ostream& operator<< (std::ostream& o, HostView<T> const& v) {
+    o << "[";
+    for(auto const& value: v) o << value << ",";
+    o << "]";
+
+    return o;
+}
+
 #ifdef WITH_CUDA
 // specialization for pinned vectors. Use a host_coordinator, because memory is
 // in the host memory space, and all of the helpers (copy, set, etc) are the
